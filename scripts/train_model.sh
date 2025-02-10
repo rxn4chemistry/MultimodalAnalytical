c5ded218af236647cc806000e8ab50c41ad8ac5a
MAlberts <marvin.alberts@ibm.com>
2025-02-10 08:49:31 -0500

    fix: update paths in scripts

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -4,26 +4,28 @@ export HF_DATASETS_CACHE=$1
 export LD_LIBRARY_PATH=/opt/share/gcc-10.1.0//lib64:/opt/share/gcc-10.1.0//lib:/usr/local/cuda-12.1/lib64
 export TOKENIZERS_PARALLELISM=False
 
-model=hf_bart_medium
+model=custom_hf_bart
 patch_size=250
 lr=5e-4
 
 top_dir=$2
+exp_dir=$3
+data_path=$4
 
 
-mkdir -p ${top_dir}/refactor_test/ir
+mkdir -p ${top_dir}/${exp_dir}
 jbsub -queue x86_24h -cores 7+1 -mem 60g \
-        -out ${top_dir}/refactor_test/ir/out.txt \
-        -err ${top_dir}/refactor_test/ir/err.txt \
+        -out ${top_dir}/${exp_dir}/out.txt \
+        -err ${top_dir}/${exp_dir}/err.txt \
         python ./src/cli/training.py\
         working_dir=${top_dir} \
-        data_path=$3 \
+        data_path=${data_path} \
         data=ir/patches \
         model=${model} \
         molecules=True \
         trainer.epochs=60 \
         model.lr=${lr} \
-        job_name=refactor_test/ir \
+        job_name= ${exp_dir}\
 
 
 
--- scripts/train_model_interactive.sh
+++ scripts/train_model_interactive.sh
@@ -4,11 +4,15 @@ export HF_DATASETS_CACHE=$1
 export LD_LIBRARY_PATH=/opt/share/gcc-10.1.0//lib64:/opt/share/gcc-10.1.0//lib:/usr/local/cuda-12.1/lib64
 export TOKENIZERS_PARALLELISM=False
 
+top_dir=$2
+exp_dir=$3
+data_path=$4
+
 
 python ./src/cli/training.py \
-    working_dir=$2 \
-    job_name=msms_cfmid_P10 \
-    data_path=$3 \
+    working_dir=${top_dir} \
+    job_name=${exp_dir} \
+    data_path=${data_path} \
     data=msms/msms_config \
     model=hf_bart_medium \
     molecules=True \
